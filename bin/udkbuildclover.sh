6f6d5540a311c8e6e7b28c4940cf2a7a4e0ca533
Canvas <1143991340@qq.com>
2018-05-25 18:20:37 +0800

    Fix a bug in udkbuildclover.sh

--- bin/udkbuildclover.sh
+++ bin/udkbuildclover.sh
@@ -12,32 +12,8 @@ if [ ! -d ~/AptioFixPkg ] ; then
 fi 
 cd ~/AptioFixPkg/ && git pull 
 
-# Custom macbuild.tool 
-cp ~/AptioFixPkg/macbuild.tool ~/AptioFixPkg/macbuild.tool.bak  
-col=`awk 'END{print NR}' ~/AptioFixPkg/macbuild.tool` 
-update1=$(cat -n ~/AptioFixPkg/macbuild.tool | grep updaterepo | grep https://github.com/tianocore/edk2 | awk '{print $1}') 
-((update2=$update1-1)) 
-((update3=$update1+1)) 
-
-sed -n "1,${update2}p" ~/AptioFixPkg/macbuild.tool > ~/AptioFixPkg/tmp.txt 
-echo -e " 
-if [ ! -d ~/AptioFixPkg/UDK ]; then 
-	mkdir UDK && cp -R ~/src/UDK2018/ ~/AptioFixPkg/UDK/ 
-	rm -rf ~/AptioFixPkg/UDK/Clover/ 
-	cd ~/AptioFixPkg/UDK/ && git pull 
-fi 
-
-" >> ~/AptioFixPkg/tmp.txt 
-sed -n "${update3},${col}p" ~/AptioFixPkg/macbuild.tool >> ~/AptioFixPkg/tmp.txt 
-cat ~/AptioFixPkg/tmp.txt > ~/AptioFixPkg/macbuild.tool 
-
 # Build AptioFixPkg by custom macbuild.tool 
 ~/AptioFixPkg/macbuild.tool 
-
-# Restore macbuild.tool 
-rm ~/AptioFixPkg/tmp.txt 
-rm ~/AptioFixPkg/macbuild.tool 
-mv ~/AptioFixPkg/macbuild.tool.bak ~/AptioFixPkg/macbuild.tool 
 
 # Copy .efi driver to Clover source dir 
 cp ~/driverefi/apfs.efi ~/src/UDK2018/Clover/CloverPackage/CloverV2/drivers-Off/drivers64UEFI/ 
